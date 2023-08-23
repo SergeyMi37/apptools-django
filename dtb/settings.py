e8d0e1f474c42d2720d7333cc69f03847e5e6751
sergeymi37 <sergey.mikhaylenko@gmail..com>
2023-08-23 16:13:50 +0300

    upd dev

--- dtb/settings.py
+++ dtb/settings.py
@@ -10,6 +10,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Load env variables from file
 dotenv_file = BASE_DIR / ".env"
