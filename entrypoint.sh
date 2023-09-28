05a72aecca53a19dcacc4c71735805ce215787a2
sergeymi37 <sergey.mikhaylenko@gmail..com>
2023-09-28 09:20:30 +0300

    add entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 python manage.py migrate
 python manage.py loaddata db-test.json
 python manage.py createsuperuser --noinput --username adm --email adm@localhost.com
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 dtb.wsgi --workers=5 --threads=1 --daemon
